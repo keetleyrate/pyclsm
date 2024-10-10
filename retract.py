20dc6ca87098838b91bdc2d2d6f5c3a6772848b9
goose-swansworth <fridgeineyes@gmail.com>
2024-10-11 12:48:02 +1300

    retract test

--- retract.py
+++ retract.py
@@ -10,10 +10,10 @@ d = 0.05
 eps = h ** (1 - d) / 2
 mesh, tree = unit_square(h)
 phi0 = box_phi(-0.5, -0.5, 0.5, 0.5, eps)
-Re = 10
+Re = 1
 We = 1
 D = 1
-M = 0.25
+M = 0.01
 solver = IncompressibleTwoPhaseFlowSolver(mesh, h, h / 10, 1, D, 1 / Re, M / Re, 1 / We, 0, phi0, d=d, c_kappa=20)
 
 no_slip = constant((0, 0), mesh, solver.velosity_space)
@@ -26,5 +26,7 @@ solver.set_y_velocity(y_equals(0), default_scalar_type(0))
 
 path = "sols/retract"
 solver.save_to_files(path, 1, 5)
-plotter = Plotter(solver, (0, 1), (0, 1), 0.1, interface_points=250, filename=path, contor_color="lightblue")
+plotter = Plotter(solver, (0, 1), (0, 1), 0.2, interface_points=0, phi_points=250, filename=path, contor_color="lightblue")
+# plotter.plot_from_solver()
+# plotter.show()
 plotter.save_to_mp4("videos/retract.mp4")
