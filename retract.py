0186604aa4ed341e1aea460741b64532352cd300
goose-swansworth <fridgeineyes@gmail.com>
2024-10-14 13:12:17 +1300

    retract sims setup

--- retract.py
+++ retract.py
@@ -3,18 +3,40 @@ from plotter import Plotter
 from twophase import IncompressibleTwoPhaseFlowSolver
 from clevelset import box_phi
 from bc import *
+from visualise import *
+from scipy import integrate
+import sys
 
-n = 100
+
+def compute_aspect(phi, n):
+    x = np.linspace(0, 1, n)
+    y = np.zeros(len(x))
+    _, _, phi_along_h = fem_scalar_func_at_given_points(phi, mesh, tree, x, y)
+    _, _, phi_along_v = fem_scalar_func_at_given_points(phi, mesh, tree, y, x)
+    a = 1 - integrate.trapz(phi_along_h.reshape(n), x)
+    b = 1 - integrate.trapz(phi_along_v.reshape(n), x)
+    return a, b
+
+
+def write_aspect(solver):
+    a, b = compute_aspect(solver.level_set.phi, 2 * n)
+    aspects.append(a / b)
+
+
+    
+Re = int(sys.argv[1])
+
+n = 25
 h = 1 / n
 d = 0.05
 eps = h ** (1 - d) / 2
 mesh, tree = unit_square(h)
-phi0 = box_phi(-0.5, -0.5, 0.5, 0.5, eps)
-Re = 1
+a = 0.95
+box = box_phi(-0.5, -0.5, a, a / 4, eps)
 We = 1
 D = 1
 M = 0.01
-solver = IncompressibleTwoPhaseFlowSolver(mesh, h, h / 10, 1, D, 1 / Re, M / Re, 1 / We, 0, phi0, d=d, c_kappa=20)
+solver = IncompressibleTwoPhaseFlowSolver(mesh, h, h / 10, 1, D, 1 / Re, M / Re, 1 / We, 0, box, d=d, c_kappa=20)
 
 no_slip = constant((0, 0), mesh, solver.velosity_space)
 
@@ -24,9 +46,18 @@ solver.set_velosity_bc(x_equals(1), no_slip)
 solver.set_x_velocity(x_equals(0), default_scalar_type(0))
 solver.set_y_velocity(y_equals(0), default_scalar_type(0))
 
-path = "sols/retract"
+aspects = []
+
+solver.set_time_step_proc(write_aspect)
+
+path = f"sols/retract-{Re}"
 solver.save_to_files(path, 1, 5)
-plotter = Plotter(solver, (0, 1), (0, 1), 0.2, interface_points=0, phi_points=250, filename=path, contor_color="lightblue")
+
+with open(f"records/aspect-{Re}.txt", "w") as infile:
+    infile.write("\n".join(map(str, aspects)))
+
+
+#plotter = Plotter(solver, (0, 1), (0, 1), 0.2, filename=path, visc_points=200, contor_color="lightblue")
 # plotter.plot_from_solver()
 # plotter.show()
-plotter.save_to_mp4("videos/retract.mp4")
+#plotter.save_to_mp4("videos/retract_long.mp4")
