ca827de6461e414338dd5cbd4989fd477134d60b
goose-swansworth <fridgeineyes@gmail.com>
2024-11-22 10:04:31 +1300

    rt step up

--- rt.py
+++ rt.py
@@ -1,19 +1,45 @@
-from domain import *
-from visualise import *
+from mesh2d import rectangle
+from plotter import Plotter
+from twophase import IncompressibleTwoPhaseFlowSolver
+from clevelset import phi, circular_level_set
 from bc import *
-from navierstokes import IncompressibleNavierStokesSolver
+from visualise import *
+from scipy import integrate
+import sys
+from common import step_until
+
+def phi0(eps):
+    def _phi0(w):
+        x, y = w[0], w[1]
+        blob = circular_level_set(0.5, 2, 0.1, eps)
+        return 1 - (1 - phi(y, 2, eps)) *  (1 - blob(w))
+    return _phi0
+
+c_normal = 0.1
 
 
 
-square = RectangularDomain(0, 1, 0, 1, 0.05)
-solver = IncompressibleNavierStokesSolver(square, 0.01, kinematic=True)
-solver.set_no_slip("L")
-solver.set_no_slip("R")
-solver.set_no_slip("B")
-solver.set_const_velocity_bc((1, 0), "T")
-solver.to_steady_state(1e-4)
+n = 128
+h = 1 / n
+d = 0.1
+eps = h ** (1 - d) / 2
+mesh, tree = rectangle((0, 0), (1, 3), h)
+a = 0.75
+box = phi0(eps)
+Re = 1
+We = 10
+D = 10
+M = 1
+Fr = 1
+solver = IncompressibleTwoPhaseFlowSolver(mesh, h, h / 10, 1, D, 1 / Re, M / Re, 1 / We, 1 / Fr**2, box, d=d, kinematic=True, c_kappa=1, c_normal=c_normal)
 
-axes = plt.axes()
-fem_plot_vectors(axes, solver.u, square.mesh, square.tree, (0, 1), (0, 1), 50)
-axes.set_aspect("equal")
-plt.show()
+no_slip = constant((0, 0), mesh, solver.velosity_space)
+
+solver.set_no_slip_everywhere()
+
+
+path = f"sols/rt-Re-{Re}-We-{We}"
+T = 50
+solver.save_to_files(path, T, 5)
+plotter = Plotter(solver, (0, 1), (0, 3), 0.2, filename=path, density_points=100, levels=100)
+plotter.save_to_mp4(f"videos/rt-Re-{Re}-We-{We}.mp4")
